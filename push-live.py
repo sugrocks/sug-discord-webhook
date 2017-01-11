edb5fe4702272b59a9440171aa44047e2fce12c6
MKody <im@kdy.ch>
2017-01-12 00:03:54 +0100

    add comments; one function to POST; image-only channels; roll for embed color

--- push-live.py
+++ push-live.py
@@ -10,49 +10,119 @@ import basc_py4chan as fch
 from collections import deque
 from datetime import datetime
 
+# count stuff
 relconf = 0
 firstrun = True
 
+# init config and list of watched threads
 config = configparser.ConfigParser()
 watching = deque('')
 
+# init 4chan boards
 co = fch.Board('co', True)
 trash = fch.Board('trash', True)
 
 
 def status_print(text):
+    # display log and don't do a line return
     sys.stdout.write('\r[{0}] {1}'.format(str(datetime.now()), str(text)))
 
 
+def roll_color(id):
+    last = int(str(id)[-1:])
+    if last == 1:
+        return 7999  # navy
+    elif last == 2:
+        return 29913  # blue
+    elif last == 3:
+        return 3066944  # green
+    elif last == 4:
+        return 3787980  # teal
+    elif last == 5:
+        return 8379391  # aqua
+    elif last == 6:
+        return 8721483  # maroon
+    elif last == 7:
+        return 11603401  # purple
+    elif last == 8:
+        return 14540253  # silver
+    elif last == 9:
+        return 16728374  # red
+    else:
+        return 16745755  # orange
+
+
+def post_discord(params, cat, hook):
+    # post to Discord
+    # set content to json and use a 'normal' UA
+    headers = {
+        'content-type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (compatible; sug/1.0)'
+    }
+    # build our webhook url
+    url = "https://discordapp.com/api/webhooks/%s/%s" % (hook, config.get(cat, hook))
+    # and build the request
+    req = urllib.request.Request(url, data=params, headers=headers)
+
+    try:
+        # make the request
+        response = urllib.request.urlopen(req)
+    except:
+        # try one more time
+        try:
+            print('\nPOSTing to Discord failed, will retry...')
+            sleep(3)
+            response = urllib.request.urlopen(req)
+        except:
+            # give up
+            return
+
+    # get content
+    rep = response.read().decode('utf8')
+    if rep != '':
+        # if we have something in return, it's not good
+        print('\n' + rep)
+
+
 def push_thread(thread, edition=''):
+    # if no edition found, just return "/sug/" and the thread number
     if edition == '':
         edition = '/sug/ no.' + thread.topic.post_id
 
+    # to make it easier
     post = thread.topic
+
+    # default
+    pushimg = False
     image = {}
     footer = {}
 
     if post.file.file_extension != 'webm' and not post.spoiler:
+        # if there's an image and it's not spoiler, add it
         image = {
             'url': post.file.file_url
         }
+        pushimg = True
     elif post.file.file_extension == 'webm':
+        # if it's a webm, add a note about that
         footer = {
             'text': '(A webm is attached)',
             'icon_url': 'https://s.kdy.ch/4ch-warning.png'
         }
     elif post.spoiler:
+        # if it was spoiled, add a note about that
         footer = {
             'text': '(Image is spoiled)',
             'icon_url': 'https://s.kdy.ch/4ch-warning.png'
         }
 
+    # and now we build the data to POST
     data = {
         'username': '/' + thread._board.name + '/',
         'embeds': [
             {
                 'title': 'New thread: ' + edition,
-                'color': 3518996,
+                'color': roll_color(post.post_id),
                 'url': thread.url,
                 'footer': footer,
                 'image': image
@@ -60,60 +130,70 @@ def push_thread(thread, edition=''):
         ]
     }
 
+    # we dump that
     params = json.dumps(data).encode('utf8')
-    headers = {
-        'content-type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (compatible; sug/1.0)'
-    }
 
+    # and we push to every concerned webhooks
     for hook in dict(config.items('new')):
-        url = "https://discordapp.com/api/webhooks/%s/%s" % (hook, config.get('new', hook))
-        req = urllib.request.Request(url, data=params, headers=headers)
+        post_discord(params, 'new', hook)
 
-        try:
-            response = urllib.request.urlopen(req)
-        except:
-            try:
-                print('\nPOSTing to Discord failed, will retry...')
-                sleep(3)
-                response = urllib.request.urlopen(req)
-            except:
-                continue
+    # if this post has an image, push to image-only channels
+    if pushimg:
+        data = {
+            'username': post.name,
+            'content': '[>>%s](%s)' % (post.post_id, post.url),
+            'embeds': [
+                {
+                    'color': roll_color(post.post_id),
+                    'image': {
+                        'url': post.file.file_url
+                    }
+                }
+            ]
+        }
+        params = json.dumps(data).encode('utf8')
 
-        rep = response.read().decode('utf8')
-        if rep != '':
-            print('\n' + rep)
+        for hook in dict(config.items(thread._board.name + 'img')):
+            post_discord(params, thread._board.name + 'img', hook)
 
 
 def push_post(post, edition=''):
+    # if no edition found, just return "/sug/" and the thread number
     if edition == '':
         edition = '/sug/ no.' + post._thread.topic.post_id
 
+    # default
+    pushimg = False
     image = {}
     footer = {}
 
     if post.has_file and post.file.file_extension != 'webm' and not post.spoiler:
+        # if there's an image and it's not spoiler, add it
         image = {
             'url': post.file.file_url
         }
+        pushimg = True
     elif post.has_file and post.file.file_extension == 'webm':
+        # if it's a webm, add a note about that
         footer = {
             'text': '(A webm is attached)',
             'icon_url': 'https://s.kdy.ch/4ch-warning.png'
         }
     elif post.spoiler:
+        # if it was spoiled, add a note about that
         footer = {
             'text': '(Image is spoiled)',
             'icon_url': 'https://s.kdy.ch/4ch-warning.png'
         }
 
+    # and now we build the data to POST
     data = {
         'username': '/' + post._thread._board.name + '/',
         'embeds': [
             {
                 'title': '%s - No.%d (%s)' % (post.name, post.post_id, edition),
                 'description': post.text_comment,
-                'color': 3518996,
+                'color': roll_color(post.post_id),
                 'url': post.url,
                 'footer': footer,
                 'image': image
@@ -121,32 +201,35 @@ def push_post(post, edition=''):
         ]
     }
 
+    # we dump that
     params = json.dumps(data).encode('utf8')
-    headers = {
-        'content-type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (compatible; sug/1.0)'
-    }
 
+    # and we push to every concerned webhooks
     for hook in dict(config.items(post._thread._board.name)):
-        url = "https://discordapp.com/api/webhooks/%s/%s" % (hook, config.get(post._thread._board.name, hook))
-        req = urllib.request.Request(url, data=params, headers=headers)
+        post_discord(params, post._thread._board.name, hook)
 
-        try:
-            response = urllib.request.urlopen(req)
-        except:
-            try:
-                print('\nPOSTing to Discord failed, will retry...')
-                sleep(3)
-                response = urllib.request.urlopen(req)
-            except:
-                continue
+    # if this post has an image, push to image-only channels
+    if pushimg:
+        data = {
+            'username': post.name,
+            'content': '[>>%s](%s)' % (post.post_id, post.url),
+            'embeds': [
+                {
+                    'color': roll_color(post.post_id),
+                    'image': {
+                        'url': post.file.file_url
+                    }
+                }
+            ]
+        }
+        params = json.dumps(data).encode('utf8')
 
-        rep = response.read().decode('utf8')
-        if rep != '':
-            print('\n' + rep)
+        for hook in dict(config.items(post._thread._board.name + 'img')):
+            post_discord(params, post._thread._board.name + 'img', hook)
 
 
 def check_config():
+    # reload config after 60 runs
     global relconf
 
     if relconf > 60:
@@ -156,51 +239,69 @@ def check_config():
 
 
 def check_sug():
+    # get data from sug.rocks about current /sug/ threads
     global relconf, firstrun
 
+    # fetch our json
     req = urllib.request.Request('https://api.sug.rocks/threads.json')
     r = urllib.request.urlopen(req).read()
     cont = json.loads(r.decode('utf-8'))
+
     for item in cont:
+        # for every /sug/ thread
         if item['board'] != 'sugen' and not item['status']['archived']:
+            # if it's not from /sugen/ and is not archived
             if not any(x['id'] == item['id'] for x in watching):
+                # ... and that we don't have it yet
                 try:
+                    # fetch the thread
                     if item['board'] == 'co':
                         thread = co.get_thread(int(item['id']), False, True)
                     elif item['board'] == 'trash':
                         thread = trash.get_thread(int(item['id']), False, True)
 
+                    # add the thread and some infos to our deque
                     print('\nAdded: ' + item['edition'])
                     watching.append({'id': item['id'], 'edition': item['edition'], 'thread': thread})
+
+                    # if it's not the first run of the script, push to concerned webhooks
                     if not firstrun:
                         push_thread(thread, item['edition'])
                 except:
                     pass
+
+    # once done, it's not the first run anymore
     firstrun = False
 
 
 def check_threads():
     toremove = []
     for w in watching:
+        # update saved threads, returns how many new posts we have
         upcount = w['thread'].update()
         if not hasattr(w['thread'], 'topic'):
+            # if there isn't any 'topic' attribute, that means the thread is dead
             toremove.append(w)
             print('\n' + str(w['id']) + ' will be removed')
         else:
+            # if there's any new posts
             if upcount > 0:
+                # get them and push to concerned webhooks
                 status_print(str(w['id']) + ': ' + str(upcount) + ' new posts')
                 newposts = w['thread'].all_posts[-upcount:]
                 for post in newposts:
                     push_post(post, w['edition'])
 
+    # remove dead threads from the main deque
     for r in toremove:
         watching.remove(r)
 
 
 if __name__ == '__main__':
     while True:
-        check_config()
-        check_sug()
-        check_threads()
-        relconf += 1
-        sleep(10)
+        # always loop
+        check_config()  # load our config if needed
+        check_sug()  # check current /sug/ threads
+        check_threads()  # get the threads and new posts
+        relconf += 1  # increment that, for reload the config
+        sleep(10)  # wait before poking everything again
